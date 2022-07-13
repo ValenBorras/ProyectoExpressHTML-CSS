56d404a1df7289f6ac9150efa4155b35e1b48517
ValenBorras <106563001+ValenBorras@users.noreply.github.com>
2022-07-13 17:50:47 -0300

    formularioRegistro

--- app.js
+++ app.js
@@ -10,3 +10,7 @@ app.listen(3000,() => console.log('Servidor corriendo...'));
 app.get('/', (req,res) =>{
     res.sendFile( path.join(__dirname, './views/index.html'))
 })
+
+app.get('/register', (req,res) =>{
+    res.sendFile(path.join(__dirname,'./views/register.html'))
+})

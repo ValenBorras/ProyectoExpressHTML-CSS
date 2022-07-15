44e3cb7351dfde96613c0603e8b43e514338fe5b
ValenBorras <106563001+ValenBorras@users.noreply.github.com>
2022-07-15 00:56:02 -0300

    main y footer hasta clase 14

--- app.js
+++ app.js
@@ -9,8 +9,12 @@ app.listen(3000,() => console.log('Servidor corriendo...'));
 
 app.get('/', (req,res) =>{
     res.sendFile( path.join(__dirname, './views/index.html'))
-})
+});
 
 app.get('/register', (req,res) =>{
     res.sendFile(path.join(__dirname,'./views/register.html'))
-})
+});
+
+app.get('/logIn', (req,res) =>{
+    res.sendFile(path.join(__dirname,'./views/logIn.html'))
+});

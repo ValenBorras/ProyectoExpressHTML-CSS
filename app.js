961374fe35e31740dd41aa7b09f75a08dc78bf59
ValenBorras <106563001+ValenBorras@users.noreply.github.com>
2022-07-17 16:51:23 -0300

    cambio puerto a 3030

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 app.use(express.static(path.resolve(__dirname, './public')));
 
-app.listen(3000,() => console.log('Servidor corriendo...'));
+app.listen(3030,() => console.log('Servidor corriendo...'));
 
 app.get('/', (req,res) =>{
     res.sendFile( path.join(__dirname, './views/index.html'))

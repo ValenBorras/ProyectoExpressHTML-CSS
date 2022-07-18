f2627313304aec10db84a3a1597f7aa05ba640ea
ValenBorras <mvalentina.borras@gmail.com>
2022-07-18 20:48:31 -0300

    gitignore

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require('express');
 const app = express();
 
 const path = require('path');
+const port = process.env.PORT || 3000;
 
 app.use(express.static(path.resolve(__dirname, './public')));
 
-app.listen(3030,() => console.log('Servidor corriendo...'));
+app.listen(port,() => console.log('Servidor corriendo...'));
 
 app.get('/', (req,res) =>{
     res.sendFile( path.join(__dirname, './views/index.html'))
